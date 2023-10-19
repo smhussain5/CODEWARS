50cf9328eaa79e23b28e8f6652a7e195e9dca3ba
smhussain5 <139831097+smhussain5@users.noreply.github.com>
2023-10-19 16:54:07 -0500

    Add Kata "Rock Paper Scissors"

---
+++ 8_kyu/Rock_Paper_Scissors.js
@@ -0,0 +1,19 @@
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+// SOLUTION BY SHABAB HUSSAIN:
+
+const rps = (p1, p2) => {
+    if (p1 === 'scissors' && p2 === 'paper' || p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock') {
+        return ('Player 1 won!')
+    } else if (p2 === 'scissors' && p1 === 'paper' || p2 === 'rock' && p1 === 'scissors' || p2 === 'paper' && p1 === 'rock') {
+        return ('Player 2 won!')
+    } else if (p1 === p2) {
+        return ('Draw!')
+    }
+};
